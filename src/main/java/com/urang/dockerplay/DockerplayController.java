417ab3eb5c50f5cef1e23907f52cd6739717896f
Park, Jinsoo | Peter | ECFD <jinsoo.park@rakuten.com>
2018-02-18 18:18:12 +0900

    Add thymeleaf and template

--- src/main/java/com/urang/dockerplay/DockerplayController.java
+++ src/main/java/com/urang/dockerplay/DockerplayController.java
@@ -2,13 +2,13 @@ package com.urang.dockerplay;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DockerplayController {
   
   @RequestMapping("/")
-  public @ResponseBody String index() {
-    return "Hello World!!!!!";
+  public String index() {
+    return "index";
   }
+  
 }
