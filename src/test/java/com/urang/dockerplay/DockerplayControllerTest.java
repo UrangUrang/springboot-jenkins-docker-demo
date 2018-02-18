c4ad723d7f5009f7445659850d569e7b0c3d96e3
Park, Jinsoo | Peter | ECFD <jinsoo.park@rakuten.com>
2018-02-18 20:09:22 +0900

    Add test cases using mockMvc

--- src/test/java/com/urang/dockerplay/DockerplayControllerTest.java
+++ src/test/java/com/urang/dockerplay/DockerplayControllerTest.java
@@ -1,16 +1,40 @@
 package com.urang.dockerplay;
 
 import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@RunWith(MockitoJUnitRunner.class)
 public class DockerplayControllerTest {
+
+  private MockMvc mockMvc;
+
+  @Before
+  public void setUp() {
+    mockMvc = MockMvcBuilders
+        .standaloneSetup(new DockerplayController())
+          .build();
+  }
+
+  @Test
+  public void index_ShowView() throws Exception {
+    mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(
+        view().name("index"));
+  }
 
   @Test
   public void index_CanWork() {
     // setup:
     DockerplayController controller = new DockerplayController();
-    String expected = "Hello World!!!!!";
+    String expected = "index";
 
     // when:
     String actual = controller.index();
